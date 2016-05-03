d5bd34ad361b5681736750de7b882b9b1b3d9c4e
towry <tovvry@gmail.com>
2016-05-03 14:16:12 +0800

    update

--- src/assets.rs
+++ src/assets.rs
@@ -1,4 +1,7 @@
 
+use std::path::{Path, PathBuf};
+use config::Config;
+
 pub struct Assets {
 
 }
@@ -9,7 +12,10 @@ impl Assets {
      * Suppose the url is: /assets/css/app-sdkj2ifak.css
      * In frontend development, we will remove the hash.
      */
-    pub fn get_asset(path: &String) -> Result<String, _> {
-        
+    pub fn get_asset(path: &String, config: &Config) -> Result<String, _> {
+        let search_path = config.search_path;
+        let resource_path = Path::from(search_path).join(path);
     }
 }
+
+fn remove_hash
