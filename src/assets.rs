d4468735494afd6af13d9bc28e44e2592f4d34d4
towry <tovvry@gmail.com>
2016-05-03 13:37:46 +0800

    remove hash

--- src/assets.rs
+++ src/assets.rs
@@ -4,6 +4,11 @@ pub struct Assets {
 }
 
 impl Assets {
+    /**
+     * path is a url path.
+     * Suppose the url is: /assets/css/app-sdkj2ifak.css
+     * In frontend development, we will remove the hash.
+     */
     pub fn get_asset(path: &String) -> Result<String, _> {
         
     }
