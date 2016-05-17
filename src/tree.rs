403faeeab1987eaa0e6685922116dc375d9f9814
towry <tovvry@gmail.com>
2016-05-17 23:50:18 +0800

    use arc instead of box

--- src/tree.rs
+++ src/tree.rs
@@ -1,24 +1,25 @@
 
 use std::io;
+use std::sync::Arc;
 use std::fs::{self, DirEntry};
 use std::path::Path;
 use util;
 
-pub struct Node<'a> {
+pub struct Node {
     pub name: String,
     pub is_dir: bool,
     pub is_file: bool,
-    child: Vec<Box<Node<'a>>>,
-    parent: Option<Box<Node<'a>>>,
+    child: Vec<Arc<Node>>,
+    parent: Option<Arc<Node>>,
     pub path: String
 }
 
-pub struct Tree<'a> {
+pub struct Tree {
     base: String,
-    tree: Option<Box<Node<'a>>>
+    tree: Option<Arc<Node>>
 }
 
-fn callback<'a>(entry: &DirEntry, parent_node: &'a mut Node<'a>) -> io::Result<()> {
+fn callback<'a>(entry: &DirEntry, parent_node: Arc<Node>) -> io::Result<()> {
     let is_file: bool;
 
     // unwrap 
@@ -28,89 +29,73 @@ fn callback<'a>(entry: &DirEntry, parent_node: &'a mut Node<'a>) -> io::Result<(
         is_file = false;
     }
 
-    let node = Node {
-        name: String::new(),
-        is_file: false,
-        is_tree: true,
-        child: Vec::new(),
-        parent: None,
-        path: String::new()
-    };
-
-    Node::new(String::from(entry.path().to_str().unwrap()), Some(parent_node), Box::new(node), is_file).unwrap();
+    Node::new(String::from(entry.path().to_str().unwrap()), Some(parent_node), is_file).unwrap();
 
     Ok(())
 }
 
-impl<'a> Node<'a> {
-    pub fn new(dir: String, mut parent: Option<Box<Node<'a>>>, mut node: Box<Node<'a>>, is_file: bool) -> Result<(), ()> {
-        let mut uw_parent = parent.unwrap();
+impl Node {
+    pub fn new(dir: String, mut parent: Option<Arc<Node>>, is_file: bool) -> Result<(Arc<Node>), ()> {
+        let node = Node {
+            name: util::path::basename(dir.clone()),
+            is_file: is_file,
+            is_dir: !is_file,
+            path: dir,
+            child: Vec::new(),
+            parent: None
+        };
 
-        node.name = util::path::basename(dir.clone());
-        node.is_file = is_file;
-        node.is_dir = !is_file;
-        node.path = dir;
+        let node_ref = Arc::new(node);
 
-        // ! 
-        // how we use uw_parent here,
-        // and use it in match ?
-        node.parent = Some(uw_parent);
-
-        match uw_parent {
-            None => {},
-            Some(_) => uw_parent.push(node),
+        match parent {
+            Some(x) => {
+                node.parent = Some(x.clone());
+                x.push(node_ref.clone());
+            },
+            None => ()
         }
 
-        // done here
         if is_file {
-            return Ok(());
+            return Ok(node_ref);
         }
 
         // if is dir 
         // read dir and create the tree 
         let path = Path::new(&dir);
-        visit_dirs(&path, &callback, uw_parent);
+        visit_dirs(&path, &callback, parent.unwrap());
 
-        return Ok(());
+        Ok(())
     }
 
-    pub fn push(&mut self, node: Box<Node<'a>>) {
+    pub fn push(&mut self, node: Arc<Node>) {
         self.child.push(node);
     }
 
-    pub fn children(&self) -> Vec<Box<Node<'a>>> {
+    pub fn children(&self) -> Vec<Arc<Node>> {
         return self.child;
     }
 }
 
-impl<'a> Tree<'a> {
-    pub fn search(&self, path: String) -> Option<Node<'a>> {
+impl Tree {
+    pub fn search(&self, path: String) -> Option<Node> {
         self.fresh();
 
         None
     }
 
     pub fn fresh(&mut self) {
-        let node = Node {
-            name: String::new(),
-            is_file: false,
-            is_tree: true,
-            child: Vec::new(),
-            parent: None,
-            path: String::new()
-        };
-        self.tree = Node::new(self.base, None, Box::new(node), false).ok();
+        self.tree = Node::new(self.base, None, false).ok();
     }
 }
 
 
-fn visit_dirs<'a> (dir: &Path, cb: &Fn(&DirEntry, &'a mut Node<'a>) -> io::Result<()>, node: &'a mut Node<'a>) -> io::Result<()> {
+fn visit_dirs<'a> (dir: &Path, cb: &Fn(&DirEntry, Arc<Node>) -> io::Result<()>, node: Arc<Node>) -> io::Result<()> {
     if try!(fs::metadata(dir)).is_dir() {
         for entry in try!(fs::read_dir(dir)) {
             let entry = try!(entry);
             cb(&entry, node);
         }
-    }
+    } 
     Ok(())
 }
 
